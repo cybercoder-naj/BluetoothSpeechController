ed6779a2170b34b1135b7314ca6795237793d1fd
cybercoder-naj <cybercoder.nishant@gmail.com>
2020-01-09 22:04:20 +0530

    Added code to input voice from user in MainActivity.kt

--- app/src/main/java/com/nishant/bluetoothspeechcontroller/MainActivity.kt
+++ app/src/main/java/com/nishant/bluetoothspeechcontroller/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.nishant.bluetoothspeechcontroller
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+private const val REQUEST_CODE = 100
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_mic.setOnClickListener {
+            getVoiceInput()
+        }
+    }
+
+    private fun getVoiceInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please speak something!")
+        try {
+            startActivityForResult(intent, REQUEST_CODE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Error while inputting speech!", Toast.LENGTH_SHORT).show()
+            Log.e(TAG, e.message!!, e)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            voice_input.text = result?.get(0) ?: "Error"
+        }
     }
 }
